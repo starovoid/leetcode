729696e1e47cc316a81aa0c26c0d1fe29a6d4162
Egor Starovoitov <52821033+PrototypeRailGun@users.noreply.github.com>
2022-02-23 14:23:04 +0300

    Create p_9.rs

---
+++ src/p_9.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn is_palindrome(mut x: i32) -> bool {
+        if x < 0 || x % 10 == 0 && x != 0 {
+            return false;
+        }
+        
+        let mut rev = 0;
+        while x > rev {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        
+        x == rev || x == rev / 10
+    }
+}
