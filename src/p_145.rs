e89f5a680fe707b50be576c45a9102b84cce3bf9
Egor Starovoitov <52821033+PrototypeRailGun@users.noreply.github.com>
2022-04-06 11:24:13 +0300

    Create p_145.rs

---
+++ src/p_145.rs
@@ -0,0 +1,36 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn postorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let mut res = Vec::new();
+        traversal(root.as_ref(), &mut res);
+        res
+    }
+}
+
+fn traversal(node_opt: Option<&Rc<RefCell<TreeNode>>>, res: &mut Vec<i32>) {
+    if let Some(node_ref) = node_opt {
+        let node = (*node_ref).borrow();
+        traversal(node.left.as_ref(), res);
+        traversal(node.right.as_ref(), res);
+        res.push(node.val);
+    }
+}
