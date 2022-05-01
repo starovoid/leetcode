f6c104273f0f96b6148d1d02725c7b246ff61288
Egor Starovoitov <52821033+PrototypeRailGun@users.noreply.github.com>
2022-05-01 10:47:54 +0300

    Create p_278.rs

---
+++ src/p_278.rs
@@ -0,0 +1,26 @@
+// The API isBadVersion is defined for you.
+// isBadVersion(version:i32)-> bool;
+// to call it use self.isBadVersion(version)
+
+impl Solution {
+    pub fn first_bad_version(&self, n: i32) -> i32 {
+		let mut left = 1;
+        let mut right = n;
+
+        while left < right {
+            let mid = left + (right - left) / 2;
+
+            if self.isBadVersion(mid) {
+                right = mid;
+            }
+            else {
+                if self.isBadVersion(mid + 1) {
+                    return mid + 1;
+                }
+                left = mid + 1;
+            }
+        }
+        
+        left
+    }
+}
