14958498d069b5a72f0f9914afdea496de5cb634
Egor Starovoitov <52821033+PrototypeRailGun@users.noreply.github.com>
2022-08-24 13:25:18 +0300

    Create p_559.py

---
+++ src/p_559.py
@@ -0,0 +1,14 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if root is None:
+            return 0
+        return max((self.maxDepth(x) for x in root.children), default=0) + 1
+        
