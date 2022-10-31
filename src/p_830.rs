168dc746bdcbe99324590597610a0205034d8190
Egor Starovoitov <52821033+starovoid@users.noreply.github.com>
2022-10-31 18:55:06 +0300

    Create p_830.rs

---
+++ src/p_830.rs
@@ -0,0 +1,24 @@
+impl Solution {
+    pub fn large_group_positions(s: String) -> Vec<Vec<i32>> {
+        let mut ans: Vec<Vec<i32>> = Vec::new();
+        let mut chars = s.chars().chain("_".chars());
+        
+        let (mut i, mut j) = (0, 0);
+        let mut p = chars.next().unwrap();
+        while let Some(c) = chars.next() {
+            if c == p {
+                j += 1;
+            } else {
+                if j - i >= 2 {
+                    ans.push(vec![i, j]);
+                }
+                i = j + 1;
+                j = i;
+                p = c;
+            }
+        }
+        
+        ans.sort_unstable_by(|a, b| a[0].cmp(&b[0]));
+        ans
+    }
+}
