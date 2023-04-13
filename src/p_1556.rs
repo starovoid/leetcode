e4bf51d81d3442417af888f504c23fa39d87cfcc
Egor Starovoitov <52821033+starovoid@users.noreply.github.com>
2023-04-13 16:37:14 +0300

    Create p_1556.rs

---
+++ src/p_1556.rs
@@ -0,0 +1,12 @@
+impl Solution {
+    pub fn thousand_separator(n: i32) -> String {
+       n.to_string()
+            .as_bytes()
+            .rchunks(3)
+            .rev()
+            .map(std::str::from_utf8)
+            .collect::<Result<Vec<&str>, _>>()
+            .unwrap()
+            .join(".")
+    }
+}
